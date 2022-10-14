ca980474b8357ef846e87988bfcfd0f81b672d67
John Roller <jtroller33@gmail.com>
2022-10-13 21:56:59 -0500

    thoughtModel done except for timestamp

--- models/thoughtModel.js
+++ models/thoughtModel.js
@@ -5,11 +5,40 @@ const formatDate = require("../utils/helpers");
 
 const thoughtSchema = new Schema(
     {
-        // thoughtText
-        // userName
-        // createdAt
-        // thoughtReactions
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: mongoose.Types.ObjectId,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            required: true,
+            default: Date.now,
+            //                  need to put timestamp here
+        },
+
+        thoughtReactions: [reactions],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
     }
 );
 
+thoughtSchema.virtual("reactionCount").get(function() {
+    return this.thoughtReactions?.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
 module.exports = Thought;
