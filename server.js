3332c96d790826d42a6b9e87921fc13cc5eb1957
John Roller <jtroller33@gmail.com>
2022-10-13 19:19:14 -0500

    starting on models

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const routes = require("./api_routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // API routes
 app.use(routes);
 
-// connect to the mongo db
+// connect to mongo db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-media_db");
 
 // start the server
