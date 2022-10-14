e8c95e9fd4d6029328482cb716f00c57239d00c7
John Roller <jtroller33@gmail.com>
2022-10-13 21:52:10 -0500

    userModel.js mostly done

--- models/userModel.js
+++ models/userModel.js
@@ -3,11 +3,42 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
     {
-        // email
-        // userName
-        // userFriends
-        
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        },
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        userThoughts: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        userFriends: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
     }
 );
 
+userSchema.virtual("friendCount").get(function () {
+    return this.userFriends?.length;
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
