d161c7d879e3f4f6e63a5d7ce99568309520fc28
John Roller <jtroller33@gmail.com>
2022-10-14 11:44:09 -0500

    reactionModel

---
+++ models/reactionModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const formatDate = require("../utils/helpers");
+
+const reactionSchema = new Schema({
+    // reactionId
+    // body
+    // userName
+    // createdAt
+    //to JSON
+    }
+);
+
+module.exports = reactionSchema;
