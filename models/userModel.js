0221158af456244859c0705d7d59228c708b9c37
John Roller <jtroller33@gmail.com>
2022-10-13 21:44:19 -0500

    userModel

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        // email
+        // userName
+        // userFriends
+        
+    }
+);
+
+module.exports = User;
