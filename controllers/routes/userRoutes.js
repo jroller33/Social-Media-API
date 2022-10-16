aa36d771cd68deb1e70b02d891bcc522a54c5d23
John Roller <jtroller33@gmail.com>
2022-10-16 13:28:43 -0500

    api routes mostly done

--- controllers/routes/userRoutes.js
+++ controllers/routes/userRoutes.js
@@ -1,25 +1,25 @@
 const router = require("express").Router();
 const userController = require("../userController");
 
-router.route("/post")
+router.route("/post")       // POST new user to db
     .post(userController.create);
 
-router.route("/")
+router.route("/")       // GET all users
     .get(userController.findAll);
 
-router.route("/:id")
+router.route("/:id")        // GET user by _id
     .get(userController.findById);
 
-router.route("/update/:id")
+router.route("/update/:id")     // PUT: update user and user's thoughts
     .put(userController.updateUser);
 
-router.route("/add/:id/friends/:friendId")
+router.route("/add/:id/friends/:friendId")      // PUT update user to add friend
     .put(userController.addFriend);
 
-router.route("/remove/:id/friends/:friendId")
+router.route("/remove/:id/friends/:friendId")   // PUT update user to remove friend
     .put(userController.removeFriend);
 
-router.route("/delete/:id")
+router.route("/delete/:id")                 // DELETE user and their thoughts
     .delete(userController.deleteUser);
 
 module.exports = router;
