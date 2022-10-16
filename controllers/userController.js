60d708ba8f84205387ceb5e46d19d886b06f6af1
John Roller <jtroller33@gmail.com>
2022-10-15 20:19:31 -0500

    most of the controllers done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,14 @@
 const db = require("../models");
 
 module.exports = {
-  create: function (req, res) { // POST new user to database
+  create: function (req, res) {   // POST: new user to db
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(500).json(err))
   },
 
-  findAll: function (req, res) { // GET all users
+  findAll: function (req, res) {    // GET all users
     db.User
       .find({})
       .populate("userThoughts")
@@ -16,8 +16,9 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(500).json(err))
   },
-  
-  findById: function (req, res) { // GET user by _id
+
+  findById: function (req, res) {   // GET a user by _id
+    db.User
       .findOne({ _id: req.params.id })
       .populate("userThoughts")
       .populate("userFriends")
@@ -25,45 +26,40 @@ module.exports = {
       .catch(err => res.status(500).json(err))
   },
 
-  updateUser: function (req, res) { // PUT user & user's Thoughts to match
+  updateUser: function (req, res) {   // PUT: user and user's thoughts
     db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      .findOneAndUpdate({ _id: req.params.id }, req.body, {new: true })
       .then(user =>
         !user
-          ? res.status(400).json({ message: "User not found" })
+          ? res.status(400).json({ message: "No user with that id" })
           : db.Thought.updateMany({ _id: { $in: user.userThoughts } },
             { userName: user.userName }
           ))
-      .then(() => res.json({ message: "User and thoughts updated" }))
+      .then(() => res.json({ message: "user's thoughts updated" }))
       .catch(err => res.status(500).json(err))
   },
 
-  
-  addFriend: function (req, res) { // PUT: update user to add friend
+  addFriend: function (req, res) {  // PUT: update user to add new friend
     db.User
-      .findOneAndUpdate({ _id: req.params.id }, { $addToSet: { userFriends: req.params.friendId } }, { new: true })
+      .findOneAndUpdate({ _id: req.params.id }, { $addToSet: { userFriends: req.params.friendId } }, {new: true })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(500).json(err))
   },
 
-  // PUT user to remove friend info
-  removeFriend: function (req, res) {
+  removeFriend: function (req, res) { // PUT: update user to remove friend
     db.User
       .findOneAndUpdate({ _id: req.params.id }, { $pull: { userFriends: req.params.friendId } }, { new: true })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(500).json(err))
   },
-
-
-  // DELETE user & user's Thoughts
-  deleteUser: function (req, res) {
+  deleteUser: function (req, res) { // DELETE user and user's thoughts
     db.User.findOneAndDelete({ _id: req.params.id })
       .then(user =>
         !user
-          ? res.status(400).json({ message: "User not found" })
-          : db.Thought.deleteMany({ _id: { $in: user.userThoughts } })
-      )
-      .then(() => res.json({ message: "User and thoughts deleted" }))
-      .catch(err => res.status(500).json(err))
+          ? res.status(400).json({ message: "no user found with that id" })
+          : db.Thought.deleteMany({ _id: { $in: user.userThoughts }})
+        )
+        .then(() => res.json({ message: "User and their thoughts deleted" }))
+        .catch(err => res.status(500).json(err))
   }
-}
+};
