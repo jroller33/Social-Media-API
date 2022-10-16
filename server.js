39737c1d1cfd7983df846e519db01fb009cba54f
John Roller <jtroller33@gmail.com>
2022-10-16 14:01:34 -0500

    server working now

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./api_routes");
+const routes = require("./controllers/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API routes
 app.use(routes);
 
-// connect to mongo db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-media_db");
 
-// start the server
 app.listen(PORT, () =>
   console.log(`Server now listening on http://localhost:${PORT}/`)
 );
